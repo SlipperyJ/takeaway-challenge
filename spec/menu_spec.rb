18c1e185d2974ce186e424d4e0a7bc21e2d49ac7
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-07-17 09:52:09 +0100

    refactor spec hash

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,10 @@
 require 'menu'
 describe Menu do
+  let(:menu_list) {{ vegan_steak: 10, carnivre_lettuce: 3, dinosaurus_burrito: 8, ostrich_tagine: 8}}
+
   context "responds to" do
     it "dishes with a list of dishes" do
-      expect(subject.dishes).to eq ({ vegan_steak: 10, carnivore_lettuce: 3, dinosaurus_burrito: 8, ostrich_tagine: 8})
+      expect(subject.dishes).to eq (menu_list)
     end
   end
 end
